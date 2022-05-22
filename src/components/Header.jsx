d94d120fff45825a056fbd2bfbc312abffdd3e8f
WBomfim <willianbomfim@hotmail.com>
2022-05-22 15:15:32 -0300

    Style: Iniciado a estrutura da estilização do componente Reader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './Header.css';
 
 class Reader extends Component {
   render() {
