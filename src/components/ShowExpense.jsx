c0863dc399ba69ccfa1e66ec5ee52fa0d6c236d8
WBomfim <willianbomfim@hotmail.com>
2022-05-23 01:04:40 -0300

    Feat: Criado o modo de exibição dinâmica para as despesas que estão no estado redux - Sempre que o estado é atualizado a lista de exibição também exibe a atualização.

--- src/components/ShowExpense.jsx
+++ src/components/ShowExpense.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './ShowExpense.css';
 
 class ShowExpense extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -18,9 +21,56 @@ class ShowExpense extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          {expenses.map((expense) => {
+            const {
+              value,
+              currency,
+              method,
+              tag,
+              description,
+              exchangeRates,
+            } = expense;
+            const currencyName = exchangeRates[currency].name;
+            const valueExchange = Number(exchangeRates[currency].ask);
+            const valueConverted = Number(value * valueExchange);
+            return (
+              <tr key={ description }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{currencyName}</td>
+                <td>{valueExchange.toFixed(2)}</td>
+                <td>{valueConverted.toFixed(2)}</td>
+                <td>Real</td>
+                <td>
+                  <button
+                    type="button"
+                  >
+                    Editar
+                  </button>
+                  <button
+                    type="button"
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default ShowExpense;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ShowExpense.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(ShowExpense);
